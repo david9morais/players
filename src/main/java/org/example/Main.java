7f74cd9445dc84c1c068a1ee0ea94f39f2850811
david9morais <david_morais@hotmail.com>
2023-07-22 18:05:46 +0100

    refactor the way has the exercise is done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,38 @@
 package org.example;
 
-import org.example.feature.Chat;
+import org.example.enums.QuestionEnum;
+import org.example.feature.GenerateMessage;
 import org.example.object.Player;
 
 public class Main {
-    private static Player player1;
-    private static Player player2;
-    private static Chat chat = new Chat();
+
+    private static GenerateMessage generateMessage = new GenerateMessage();
 
     public static void main(String[] args) {
-        player1 = new Player("Interviewer");
-        player2 = new Player("Interviewee");
+        Player player1 = new Player("Interviewer");
+        Player player2 = new Player("Interviewee");
 
-        chat.startChat(player1, player2);
+        System.out.println("The interview will start!");
+
+        Player initiator = player1; // Initiator is Player 1
+        Player receiver = player2;
+
+        String message = "Hi " + receiver.getName();
+
+        do {
+            String reply = initiator.sendMessage(message);
+            System.out.println("\n" + initiator.getName() + " : " + reply);
+            initiator = (initiator == player1) ? player2 : player1; // Switch initiator
+            receiver = (receiver == player2) ? player1 : player2; // Switch receiver
+            if (initiator.getMessagesSent() == 1) {
+                message = QuestionEnum.FIRST.getQuestion();
+            }
+            if (initiator.getMessagesSent() >= 1 && receiver.getMessagesSent() >= 2 && initiator.getMessagesSent() < 10) {
+                message = (initiator == player1) ? generateMessage.generateQuestion() : generateMessage.generateResponse(message);
+            }
+        } while (initiator.getMessagesSent() < 10);
+
+        System.out.println("The interview is over.");
+        System.exit(0);
     }
 }
--- src/main/java/org/example/object/Player.java
+++ src/main/java/org/example/object/Player.java
@@ -3,11 +3,11 @@ package org.example.object;
 public class Player {
 
     private String name;
-
-    private int messagesSent;
+    private int messagesSent; // store the number of messages already sent
 
     public Player(String name) {
         this.name = name;
+        this.messagesSent = 0;
     }
 
     public String getName() {
@@ -18,7 +18,9 @@ public class Player {
         return messagesSent;
     }
 
-    public void setMessagesSent(int messagesSent) {
-        this.messagesSent = messagesSent;
+    // method that receives a message and print it wiht the number of messages sent by the player
+    public String sendMessage(String message) {
+        String numberMessages = messagesSent < 1 ? " message)" : " messages)";
+        return message + " - (" + name + " already sent " + (++messagesSent) + numberMessages + "\n";
     }
 }
